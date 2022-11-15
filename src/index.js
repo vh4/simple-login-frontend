9002b397de53c81fed1ed81aa6b5dda851fd33c4
Fathoni waseso jati <35059196+vh4@users.noreply.github.com>
2022-11-15 15:44:27 +0700

    cors update frontend done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import axios from "axios";
+import axios from "axios";
 import App from "./App.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min'
