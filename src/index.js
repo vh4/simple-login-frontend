c9dbe9a222b77e965da26bf89bc81fdc874a01c3
Fathoni waseso jati <35059196+vh4@users.noreply.github.com>
2022-11-15 13:31:01 +0700

    update credentials

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import axios from "axios";
+// import axios from "axios";
 import App from "./App.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
